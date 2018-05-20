b905ab90563c8fd2d43edc0bb5222de600d4e3dc
Justin Lu <matrix0222@gmail.com>
2018-05-19 21:00:07 -0400

    Created car node init method, got rid of dumb try catch statement

--- CompSciFinal/src/application/FinalProject.java
+++ CompSciFinal/src/application/FinalProject.java
@@ -1,38 +1,58 @@
 package application;
 	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 
 
 public class FinalProject extends Application {
 	
-	@Override
-	public void start(Stage primaryStage) {
+	public static final double pane_width = 500;
+	public static final double pane_height = 500;
+	
+	private Rectangle[] initCars(int num, double width, double height) {
 		
-		Button butt = new Button("Mello World!");
-		Scene scene = new Scene(butt, 200, 250);
+		Rectangle[] cars = new Rectangle[num];
+		
+		for (int i = 0; i < cars.length; i++) {
+			cars[i] = new Rectangle(width, height, Color.DARKRED);
+			cars[i].setStroke(Color.RED);
+			cars[i].setY(i * height + i * 1);	
+		}
+		
+		return cars;
+	}
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Rectangle rect = new Rectangle(50, 25, Color.RED);
+		
+		Pane root = new Pane();
+		
+		Rectangle[] vehicles = initCars(5, 50, 25);
+		
+		for (int i = 0; i < vehicles.length; i++) {
+			root.getChildren().add(vehicles[i]);
+		}
+		
+		Scene scene = new Scene(root, pane_width, pane_height);
 		
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("My JavaFX");
 		primaryStage.show();
 		
-//		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root,400,400);
-//			
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			
-//			primaryStage.setTitle("Success");
-//			primaryStage.show();
-//			
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
 	}
+	
 	
 	public static void main(String[] args) {
 		Application.launch(args);
