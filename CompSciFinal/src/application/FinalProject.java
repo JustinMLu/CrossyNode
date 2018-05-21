1016cb2f9e9bea5b4a8bfff10e9abde3220d0958
Justin Lu <matrix0222@gmail.com>
2018-05-21 13:39:43 -0400

    - Created frog init method and event handler for moving frog node
    - Added a random-row car init method
    
    - Added an animationTimer that doesn't work

--- CompSciFinal/src/application/FinalProject.java
+++ CompSciFinal/src/application/FinalProject.java
@@ -1,53 +1,107 @@
 package application;
 	
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import javafx.animation.AnimationTimer;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.control.Button;
+import javafx.scene.effect.Glow;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 
 public class FinalProject extends Application {
 	
 	public static final double pane_width = 500;
 	public static final double pane_height = 500;
+	public static final double frog_size = 25;
+
+	private AnimationTimer timer;
+	private Pane root;
+	private Rectangle frog;
+	private Rectangle car;
+	private ArrayList<Rectangle> cars = new ArrayList<Rectangle>();
 	
-	private Rectangle[] initCars(int num, double width, double height) {
+	private Rectangle initCar() {
+		Rectangle car = new Rectangle(50, 25, Color.DARKRED);
 		
-		Rectangle[] cars = new Rectangle[num];
-		
-		for (int i = 0; i < cars.length; i++) {
-			cars[i] = new Rectangle(width, height, Color.DARKRED);
-			cars[i].setStroke(Color.RED);
-			cars[i].setY(i * height + i * 1);	
-		}
-		
-		return cars;
+		car.setStroke(Color.RED);
+		car.setTranslateY((int) (Math.random() * (pane_height / frog_size - 1)) * frog_size); //number of rows - 1 * size of frog (courtesy of tutorial guy)
+	
+		return car;
 	}
 	
+	private Rectangle initFrog(int moveAmount) {
+		
+		Rectangle froggo = new Rectangle(frog_size, frog_size, Color.DARKOLIVEGREEN);
+		
+		froggo.setStroke(Color.FORESTGREEN);
+		froggo.setY(pane_height - frog_size);
+		froggo.setX((pane_width / 2) - frog_size);
+		
+		return froggo;
+	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+
+		root = new Pane();
+		frog = initFrog(20);
+		car = initCar();
 		
-		Rectangle rect = new Rectangle(50, 25, Color.RED);
+		//add the frog to the root node
+		root.getChildren().add(frog);
+		root.getChildren().add(car);
 		
-		Pane root = new Pane();
-		
-		Rectangle[] vehicles = initCars(5, 50, 25);
-		
-		for (int i = 0; i < vehicles.length; i++) {
-			root.getChildren().add(vehicles[i]);
-		}
+		//WHY DOESNT THIS WORK
+		timer = new AnimationTimer() {
+			@Override
+			public void handle(long now) { //repeatedly initialize and animate cars
+				car.setTranslateX(car.getTranslateX() + (Math.random() * 10));
+			}
+		};
 		
 		Scene scene = new Scene(root, pane_width, pane_height);
 		
 		primaryStage.setScene(scene);
+		
+		primaryStage.getScene().setOnKeyPressed(event -> {
+			switch (event.getCode()) {
+			case UP:
+				frog.setTranslateY(frog.getTranslateY() - frog_size); // moves the frog 1 frog size up/down/left/right
+				break;
+				
+			case DOWN:
+				frog.setTranslateY(frog.getTranslateY() + frog_size);
+				break;
+				
+			case LEFT:
+				frog.setTranslateX(frog.getTranslateX() - frog_size);
+				break;
+				
+			case RIGHT:
+				frog.setTranslateX(frog.getTranslateX() + frog_size);
+				break;
+				
+			default:
+				break;
+			}	
+		});
+		
 		primaryStage.setTitle("My JavaFX");
 		primaryStage.show();
 		
