13c643a76c725b6b8c20412b614136e1e839b195
Justin Lu <matrix0222@gmail.com>
2018-05-21 18:18:42 -0400

    COMPLETED BASE FRAMEWORK LOGIC FOR ENTIRE GAME!!!!
    - Added frog/car collision check and game over condition
    
    - Added Pane Boundary conditions for moving frog (frog cannot move
    offscreen)
    
    - Fixed random car generation and speed
    
    - Moved root node, scene, and frog initialization to generateElements
    method
    
    - Fixed sizing of frog and vehicles so that collisions don't bug out
    when car passes in front/behind frog

--- CompSciFinal/src/application/FinalProject.java
+++ CompSciFinal/src/application/FinalProject.java
@@ -1,6 +1,7 @@
 package application;
 	
 
+import java.awt.Paint;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -28,78 +29,113 @@ public class FinalProject extends Application {
 	
 	public static final double pane_width = 500;
 	public static final double pane_height = 500;
-	public static final double frog_size = 25;
+	public static final double max_size = 25; //maximum size for every visual node 
 
 	private AnimationTimer timer;
 	private Pane root;
 	private Rectangle frog;
-	private Rectangle car;
+	private boolean stopCondition = false;
 	private ArrayList<Rectangle> cars = new ArrayList<Rectangle>();
+
 	
 	private Rectangle initCar() {
-		Rectangle car = new Rectangle(50, 25, Color.DARKRED);
+		Rectangle car = new Rectangle(max_size * 2, max_size - 1, Color.DARKRED);
 		
 		car.setStroke(Color.RED);
-		car.setTranslateY((int) (Math.random() * (pane_height / frog_size - 1)) * frog_size); //number of rows - 1 * size of frog (courtesy of tutorial guy)
+		car.setTranslateY((int) (Math.random() * (pane_height / max_size - 1)) * max_size); //number of rows - 1 * size of frog
 	
+		root.getChildren().add(car);
 		return car;
 	}
 	
+	
 	private Rectangle initFrog(int moveAmount) {
-		
-		Rectangle froggo = new Rectangle(frog_size, frog_size, Color.DARKOLIVEGREEN);
+		Rectangle froggo = new Rectangle(max_size - 3, max_size - 3, Color.DARKOLIVEGREEN);
 		
 		froggo.setStroke(Color.FORESTGREEN);
-		froggo.setY(pane_height - frog_size);
-		froggo.setX((pane_width / 2) - frog_size);
+		froggo.setY((pane_height - max_size) + 1);
+		froggo.setX((pane_width / 2) - max_size);
+		
+		//SETS THE DEFAULT 0,0 POINT TO WHERE THE FROG SPAWNS
+		froggo.setTranslateX(0);
+		froggo.setTranslateY(0);
 		
 		return froggo;
 	}
 	
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-
+	
+	private void spawnCars() {
+		//RANDOMLY SPAWNS CARS
+		for (Rectangle car : cars) {
+			
+			car.setTranslateX(car.getTranslateX() + (Math.random() * 5));
+			
+			if (car.getBoundsInParent().intersects(frog.getBoundsInParent())) {
+				//timer.stop();
+				stopCondition = true;
+				frog.setFill(Color.DARKRED);
+				frog.setStroke(Color.DARKRED);
+			}
+		}
+		
+		if ((int) (Math.random() * 100) < 2 && !stopCondition) {
+			cars.add(initCar());
+		}	
+	}
+	
+	private Pane generateElements() {
 		root = new Pane();
 		frog = initFrog(20);
-		car = initCar();
 		
 		//add the frog to the root node
 		root.getChildren().add(frog);
-		root.getChildren().add(car);
 		
-		//WHY DOESNT THIS WORK
 		timer = new AnimationTimer() {
+			
 			@Override
 			public void handle(long now) { //repeatedly initialize and animate cars
-				car.setTranslateX(car.getTranslateX() + (Math.random() * 10));
+				spawnCars();
 			}
 		};
+		timer.start();
 		
-		Scene scene = new Scene(root, pane_width, pane_height);
+		return root;
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Scene scene = new Scene(generateElements(), pane_width, pane_height);
 		
 		primaryStage.setScene(scene);
 		
 		primaryStage.getScene().setOnKeyPressed(event -> {
+			
 			switch (event.getCode()) {
 			case UP:
-				frog.setTranslateY(frog.getTranslateY() - frog_size); // moves the frog 1 frog size up/down/left/right
-				break;
+				if (!stopCondition && frog.getTranslateY() > -475)
+					frog.setTranslateY(frog.getTranslateY() - max_size); // moves the frog 1 frog size up/down/left/right
+					break;
 				
 			case DOWN:
-				frog.setTranslateY(frog.getTranslateY() + frog_size);
-				break;
+				if (!stopCondition && frog.getTranslateY() < 0)
+					frog.setTranslateY(frog.getTranslateY() + max_size);
+					break;
 				
 			case LEFT:
-				frog.setTranslateX(frog.getTranslateX() - frog_size);
-				break;
+				if (!stopCondition && frog.getTranslateX() > (-pane_width / 2 + max_size))
+					frog.setTranslateX(frog.getTranslateX() - max_size);
+					break;
 				
 			case RIGHT:
-				frog.setTranslateX(frog.getTranslateX() + frog_size);
-				break;
+				if (!stopCondition && frog.getTranslateX() < pane_width / 2)
+					frog.setTranslateX(frog.getTranslateX() + max_size);
+					break;
 				
 			default:
 				break;
-			}	
+			}			
+			//System.out.println("Translate X & Y: " + frog.getTranslateX() + " " + frog.getTranslateY());
 		});
 		
 		primaryStage.setTitle("My JavaFX");
